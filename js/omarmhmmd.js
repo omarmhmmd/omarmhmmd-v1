5bfca8d332b8e275391c5e8f0c86c391a2a9b61d
omarmhmmd <omar.mhmmd@gmail.com>
2017-10-23 22:22:12 -0700

    animated menu

--- js/omarmhmmd.js
+++ js/omarmhmmd.js
@@ -5,7 +5,8 @@ for (var i = 0; i < x.length; i++) {
   x[i].style.backgroundColor = "#FFF";
 }
 
-var duration = 2000;
+var durationColors = 2000;
+var durationLayers = 500;
 var easing = 'easeInOutQuad';
 var h = Math.floor(Math.random()*361);
 var s = 75;
@@ -15,35 +16,35 @@ window.onload = function() {
   var layer001 = anime({
     targets: '.layer001',
     backgroundColor: 'hsl(' + h + ',' + s + '%, 50%)',
-    duration: duration,
+    duration: durationColors,
     delay:0,
     easing:easing
   });
   var layer002 = anime({
     targets: '.layer002',
     backgroundColor: 'hsl(' + h + ',' + s + '%, 56.7%)',
-    duration: duration,
+    duration: durationColors,
     delay:500,
     easing:easing
   });
   var layer003 = anime({
     targets: '.layer003',
     backgroundColor: 'hsl(' + h + ',' + s + '%, 62.4%)',
-    duration: duration,
+    duration: durationColors,
     delay:1000,
     easing:easing
   });
   var layer004 = anime({
     targets: '.layer004',
     backgroundColor: 'hsl(' + h + ',' + s + '%, 69.1%)',
-    duration: duration,
+    duration: durationColors,
     delay:1500,
     easing:easing
   });
   var layer005 = anime({
     targets: '.layer005',
     backgroundColor: 'hsl(' + h + ',' + s + '%, 75.8%)',
-    duration: duration,
+    duration: durationColors,
     delay:2000,
     easing:easing
   });
@@ -51,7 +52,7 @@ window.onload = function() {
   var layer006 = anime({
     targets: '.layer006',
     backgroundColor: 'hsl(' + h + ',' + s + '%, 82.5%)',
-    duration: duration,
+    duration: durationColors,
     delay:3000,
     easing:easing
   });
@@ -59,8 +60,8 @@ window.onload = function() {
   var layer007 = anime({
     targets: '.layer007',
     backgroundColor: 'hsl(' + h + ',' + s + '%, 89.2%)',
-    duration: duration,
-    delay:3000,
+    duration: durationColors,
+    delay:3500,
     easing:easing
   });
 }
@@ -105,3 +106,79 @@ dw_Tooltip.content_vars = {
     }
 }
 /******** END TOOLTIP ********/
+
+/******** LAYER 001 ********/
+var clickedLayer001 = false;
+$("button").click(function() {
+  if (!clickedLayer001) {
+    $(".layer001").removeClass("L1");
+    $(".layer002").css("display", "none");
+    $(".layer003").css("display", "none");
+    $(".layer004").css("display", "none");
+    $(".layer005").css("display", "none");
+    $(".layer006").css("display", "none");
+    $(".layer007").css("display", "none");
+
+    $(".layer001").css({borderBottom: 'solid #000 0.15vw'}).animate({
+         borderWidth: "0.15vw"
+     }, 500);
+    var animation = anime({
+      targets: '.layer001',
+      translateY: "-125%",
+      easing:easing,
+      duration:durationLayers,
+    });
+    var animation = anime({
+      targets: 'body',
+      backgroundColor: ['#FFF', 'hsl(' + h + ',' + s + '%, 50%)'],
+      //delay: durationLayers * 2,
+      easing:easing,
+      duration:durationColors / 2
+    });
+    var animation = anime({
+      targets: 'button',
+      rotate: 45,
+      scale: 1.25,
+      easing:easing,
+      duration:durationLayers
+    });
+    clickedLayer001 = true;
+  }
+  else if (clickedLayer001) {
+    console.log("back");
+    $(".layer001").addClass("L1");
+    $(".layer002").css("display", "");
+    $(".layer003").css("display", "");
+    $(".layer004").css("display", "");
+    $(".layer005").css("display", "");
+    $(".layer006").css("display", "");
+    $(".layer007").css("display", "");
+
+    $(".layer001").css({borderBottom: 'solid #000 0vw'}).animate({
+         borderWidth: "0vw"
+     }, 500);
+    var animation = anime({
+      targets: '.layer001',
+      translateY: "0",
+      easing:easing,
+      duration:durationLayers,
+    });
+    var animation = anime({
+      targets: 'body',
+      backgroundColor: ['hsl(' + h + ',' + s + '%, 50%)', '#FFF'],
+      //delay: durationLayers * 2,
+      easing:easing,
+      duration:durationColors / 2
+    });
+    var animation = anime({
+      targets: 'button',
+      rotate: -90,
+      scale: 1,
+      easing:easing,
+      duration:durationLayers
+    });
+    clickedLayer001 = false;
+  }
+
+})
+/******** END LAYER 001 ********/
