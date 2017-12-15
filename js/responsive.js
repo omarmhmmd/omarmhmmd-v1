e868c09e638b97df0c2b34e7d0d02bd88fc07d86
omarmhmmd <omar.mhmmd@gmail.com>
2017-12-15 01:15:34 -0800

    mobile message 002

--- js/responsive.js
+++ js/responsive.js
@@ -2,8 +2,11 @@ console.log("hi");
 if($(window).width() < $(window).height()) {
   console.log("mobile");
     $('html').css("overflow", "hidden");
-    $('ul').css("padding-bottom", "22%");
-    $('.title h1').css("font-size", "2.25vh");
+    $('ul').css("padding-bottom", "23.5%");
+    $('.title h1').css("font-size", "5.25vh");
+    $('.title h1').css("position", "absolute");
+    $('.title h1').css("line-height", "275%");
     document.getElementById("title").innerHTML = "Omar Mohammad is an artist, designer and progammer but it would be cool if you viewed his work on a real screen.";
     document.getElementsByTagName("button").disabled = true;
+    $('.menu').css("margin-top", "-2.5%");
 }
