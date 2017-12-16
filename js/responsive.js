30cffa57eebfb9be579b22843a13ae9fe66ccc6d
omarmhmmd <omar.mhmmd@gmail.com>
2017-12-16 11:59:12 -0800

    mobile message 004

--- js/responsive.js
+++ js/responsive.js
@@ -1,13 +1,5 @@
-console.log("hi");
 if($(window).width() < $(window).height()) {
   console.log("mobile");
-    $('html').css("overflow", "hidden");
-    $('ul').css("padding-bottom", "23.5%");
-    $('.title h1').css("font-size", "5.25vh");
-    $('.title h1').css("position", "absolute");
-    $('.title h1').css("line-height", "275%");
-    document.getElementById("title").innerHTML = "Omar Mohammad </br> is an artist, designer and progammer but it would be cool if you viewed his work on a real screen.";
+    document.getElementById("title").innerHTML = "Omar Mohammad is an artist/designer/progammer <br> but it would be cool if you viewed his work on a real screen. For now you can see what he's up to on the <a style = 'text-decoration: underline; font-family:nh55 !important;' target = '_blank' href= 'https://www.instagram.com/flwrboi/'>internet</a> or you can send him an <a style = 'text-decoration: underline; font-family:nh55 !important;' href='mailto:omar.mhmmd@gmail.com'>e-mail</a>.";
     document.getElementsByTagName("button").disabled = true;
-    $('.menu').css("margin-top", "-2.5%");
-    $(".layers ul").empty();
 }
