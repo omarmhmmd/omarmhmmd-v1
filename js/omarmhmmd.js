3fbb2500b1eb9a1d3d1e0ebbf30dd8f16f12813f
omarmhmmd <omar.mhmmd@gmail.com>
2017-12-18 00:23:31 -0800

    final 003

--- js/omarmhmmd.js
+++ js/omarmhmmd.js
@@ -50,9 +50,50 @@ for (var i = 0; i < x.length; i++) {
 var durationColors = 2000;
 var durationLayers = 1000;
 var easing = 'easeInOutQuad';
-var h = Math.floor(Math.random()*361);
-var s =Math.floor(Math.random()*101);
+var h;
+var s
+function newColor() {
+  h = Math.floor(Math.random()*361);
+  s = Math.floor(Math.random()*101);
+}
+newColor();
 var layerFadeIn = 750;
+
+$(".layer007").click(function() {
+  window.location.reload(false);
+});
+
+function hslToHex(h, s, l) {
+  h /= 360;
+  s /= 100;
+  l /= 100;
+  let r, g, b;
+  if (s === 0) {
+    r = g = b = l; // achromatic
+  } else {
+    const hue2rgb = (p, q, t) => {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    };
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+  const toHex = x => {
+    const hex = Math.round(x * 255).toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  };
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+}
+
+console.log(hslToHex(h, s, 50));
+document.getElementById("changeColor").innerHTML = hslToHex(h, s, 50);
 
 // var color001 = 'hsl(' + h + ',' + s + '%, 50%)';
 // var color002 = 'hsl(' + h + ',' + s + '%, 56.7%)';
